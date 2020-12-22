36a074641178a535d5e92f32798f3da4304f0932
cedric <cedric.basuel@gmail.com>
2020-12-22 14:32:48 +0800

    just cleaned some parts

--- app.py
+++ app.py
@@ -10,35 +10,15 @@ app.config['UPLOAD_PATH'] = 'uploads'
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    
-    # uri=None
-    if request.method=='POST':
 
-        # if True:
-        #     img = request.files['file']
+    if request.method=='POST':
         for img in request.files.getlist('file'):
             img.save(os.path.join(app.config['UPLOAD_PATH'], img.filename))
-            # img1 = np.frombuffer(img.read(), np.uint8)
-            # img1 = cv2.imdecode(img1, cv2.IMREAD_COLOR)
-            # img1 = Image.fromarray(img1.astype("uint8"))
-            # rawBytes = io.BytesIO()
-            # img1.save(rawBytes, "JPEG")
-            # rawBytes.seek(0)
-            # img1_base64 = b64encode(rawBytes.getvalue()).decode('utf-8') 
-
-            # encoded = b64encode(img.stream)
-            # mime = "image/jpeg"
-            # uri = "data:%s;base64,%s" % (mime, img1_base64)
-            
-            # images.append(uri)
-
         return redirect(url_for('index'))
     
     images = os.listdir(app.config['UPLOAD_PATH'])
 
-
     return render_template('index.html', images=images, cluster_dist={})
-    # return render_template('index.html',uri=uri)
 
 @app.route('/uploads/<filename>')
 def upload(filename):
@@ -60,8 +40,6 @@ def predict():
         image_list, emb_list = get_embedding(image_list=image_list,
         model_name='mobilenet',
         image_shape=(224,224,3))
-
-        # print(emb_list.shape)
 
         clustered_images = cluster_images(image_list=image_list, 
         emb_list=emb_list, 
